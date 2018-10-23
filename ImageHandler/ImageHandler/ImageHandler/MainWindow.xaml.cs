c15ea54a2f11bf661112c32eff3c23a27cc7b75c
vkoktashev <vladislav54415@gmail.com>
2018-10-23 22:18:02 +0700

    1

--- ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
+++ ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace ImageHandler
+﻿using System.Net.Mime;
+using System.Windows;
+using System.Windows.Controls;
+using System.Drawing;
+using System;
+
+namespace ImageHandler
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +14,15 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ImageClicked(object sender, RoutedEventArgs e)
+        {
+            Image selectedImage = (Image) e.Source;
+            sizeOfImage.Content = "Размер файла в пикселях: " + (MainImage.ActualHeight * MainImage.ActualWidth);
+            resolutionOfImage.Content = "Разрешение файла: " + selectedImage.ActualWidth + "x" + selectedImage.ActualHeight;
+            MainImage.Source = selectedImage.Source;
+            
         }
     }
 }
