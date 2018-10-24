d4ec55694b0042d1367e9985a90d84f0decfac28
AlexeyCanyon <aleks230222222@yandex.ru>
2018-10-24 23:03:34 +0700

    Поиск кол-ва различных цветов в картинке

--- ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
+++ ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Mime;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
 using System.Windows;
 using System.Windows.Controls;
 using System.Drawing;
@@ -23,10 +25,23 @@ namespace ImageHandler
             MainImage.Source = selectedImage.Source;
             Image sourceBitmap = Image.FromFile(MainImage.Source.ToString().Substring(8));
             Bitmap bmp = new Bitmap(MainImage.Source.ToString().Substring(8));
+            List<string> listColors = new List<string>();
+            string pixelColor;
+            for (int i = 0; i < sourceBitmap.Width-1; i++)
+            {
+                for (int j = 0; j < sourceBitmap.Height-1; j++)
+                {
+                    pixelColor = ColorTranslator.ToHtml(bmp.GetPixel(i,j));
+                    if (listColors.IndexOf(pixelColor) == -1)
+                    {
+                        listColors.Add(pixelColor);
+                    }
+                    
+                }
+            }
             
-            
-            string pixelColor = ColorTranslator.ToHtml(bmp.GetPixel(225,7)); //x-в пикселях , y - в пикселях
-            currentPixel.Content = "Цвет" + pixelColor;
+             //x-в пикселях , y - в пикселях
+            currentPixel.Content = "Различных цветов в картинке: " + listColors.Count;
             
             
             sizeOfImage.Content = "Размер файла в пикселях: " + (sourceBitmap.Height * sourceBitmap.Width);
