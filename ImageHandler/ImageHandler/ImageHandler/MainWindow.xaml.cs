7d1b8ecf62962b5b1e28aab51c7224592d137c53
AlexeyCanyon <aleks230222222@yandex.ru>
2018-10-24 22:26:40 +0700

    Захордкоженный гетПиксель

--- ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
+++ ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
@@ -22,6 +22,13 @@ namespace ImageHandler
             ImageWPF selectedImage = (ImageWPF) e.Source;
             MainImage.Source = selectedImage.Source;
             Image sourceBitmap = Image.FromFile(MainImage.Source.ToString().Substring(8));
+            Bitmap bmp = new Bitmap(MainImage.Source.ToString().Substring(8));
+            
+            
+            string pixelColor = ColorTranslator.ToHtml(bmp.GetPixel(225,7)); //x-в пикселях , y - в пикселях
+            currentPixel.Content = "Цвет" + pixelColor;
+            
+            
             sizeOfImage.Content = "Размер файла в пикселях: " + (sourceBitmap.Height * sourceBitmap.Width);
             resolutionOfImage.Content = "Разрешение файла: " + sourceBitmap.Width + "x" + sourceBitmap.Height;
         }
