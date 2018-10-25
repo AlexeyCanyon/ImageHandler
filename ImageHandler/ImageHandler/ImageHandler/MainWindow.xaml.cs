99662acd9f271ed728763d69d99a55af37186cad
vkoktashev <vladislav54415@gmail.com>
2018-10-25 20:58:33 +0700

    5

--- ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
+++ ImageHandler/ImageHandler/ImageHandler/MainWindow.xaml.cs
@@ -4,8 +4,14 @@ using System.Net.Mime;
 using System.Windows;
 using System.Windows.Controls;
 using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Forms;
+using System.IO;
+using HorizontalAlignment = System.Windows.HorizontalAlignment;
 using Image = System.Drawing.Image;
 using ImageWPF = System.Windows.Controls.Image;
+using MessageBox = System.Windows.MessageBox;
+
 
 namespace ImageHandler
 {
@@ -24,24 +30,24 @@ namespace ImageHandler
             ImageWPF selectedImage = (ImageWPF) e.Source;
             MainImage.Source = selectedImage.Source;
             Image sourceBitmap = Image.FromFile(MainImage.Source.ToString().Substring(8));
-            Bitmap bmp = new Bitmap(MainImage.Source.ToString().Substring(8));
-            List<string> listColors = new List<string>();
-            string pixelColor;
-            for (int i = 0; i < sourceBitmap.Width-1; i++)
-            {
-                for (int j = 0; j < sourceBitmap.Height-1; j++)
-                {
-                    pixelColor = ColorTranslator.ToHtml(bmp.GetPixel(i,j));
-                    if (listColors.IndexOf(pixelColor) == -1)
-                    {
-                        listColors.Add(pixelColor);
-                    }
-                    
-                }
-            }
+//            Bitmap bmp = new Bitmap(MainImage.Source.ToString().Substring(8));
+//            List<string> listColors = new List<string>();
+//            string pixelColor;
+//            for (int i = 0; i < sourceBitmap.Width-1; i++)
+//            {
+//                for (int j = 0; j < sourceBitmap.Height-1; j++)
+//                {
+//                    pixelColor = ColorTranslator.ToHtml(bmp.GetPixel(i,j));
+//                    if (listColors.IndexOf(pixelColor) == -1)
+//                    {
+//                        listColors.Add(pixelColor);
+//                    }
+//                    
+//                }
+//            }
             
              //x-в пикселях , y - в пикселях
-            currentPixel.Content = "Различных цветов в картинке: " + listColors.Count;
+//            colorsCount.Content = "Различных цветов в картинке: " + listColors.Count;
             
             
             sizeOfImage.Content = "Размер файла в пикселях: " + (sourceBitmap.Height * sourceBitmap.Width);
@@ -50,6 +56,27 @@ namespace ImageHandler
         
         private void AddPictures(object sender, RoutedEventArgs e)
         {
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter =
+                "Images (*.BMP;*.JPG)|*.BMP;*.JPG";
+
+            openFileDialog1.Multiselect = true;
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    ImageWPF image = new ImageWPF();
+                    BitmapImage bi3 = new BitmapImage();
+                    bi3.BeginInit();
+                    bi3.UriSource = new Uri(file);
+                    bi3.EndInit();
+                    image.Source = bi3;
+                    image.Width = 350;
+                    image.HorizontalAlignment = HorizontalAlignment.Center;
+                    imagesPanel.Children.Add(image);
+                }
         }
+        
+        
     }
 }
