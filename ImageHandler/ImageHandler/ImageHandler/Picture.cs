4cda3c20bfdf0b3927dd334d77a19ab2c6ad52f3
AlexeyCanyon <aleks230222222@yandex.ru>
2018-11-17 12:15:34 +0700

    Изменения типа координат

--- ImageHandler/ImageHandler/ImageHandler/Picture.cs
+++ ImageHandler/ImageHandler/ImageHandler/Picture.cs
@@ -4,9 +4,15 @@ namespace ImageHandler
     {
         public string name;
         public int yearOfCreation;
-        public string coordinates;
-        public string  placeOfCreation;
-        public string placeOfStorage;
+        public float longitude;
+        public float latitude;
+        
+        public float  longitudeCreation;
+        public float  latitudeCreation;
+        
+        public float longitudeStorage;
+        public float latitudeStorage;
+        
         public int height;
         public int width;
         public string rules;
