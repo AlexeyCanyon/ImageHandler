c4187f9433218a04a62b3dd9289c2f585ccfe7d4
AlexeyCanyon <aleks230222222@yandex.ru>
2018-11-17 11:50:39 +0700

    Добавление класса Picture

---
+++ ImageHandler/ImageHandler/ImageHandler/Picture.cs
@@ -0,0 +1,16 @@
+namespace ImageHandler
+{
+    public class Picture
+    {
+        public string name;
+        public int yearOfCreation;
+        public string coordinates;
+        public string  placeOfCreation;
+        public string placeOfStorage;
+        public int height;
+        public int width;
+        public string rules;
+        public string description;
+        
+    }
+}
